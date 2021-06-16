2de9b4b9fede5214fa462a1307abab9e46745e59
Ethan Simon-Law <ethan.simon.3d@gmail.com>
2021-06-16 15:59:46 -0400

    Fix for Add ID to Selected pt.2

--- mat_id_ops.py
+++ mat_id_ops.py
@@ -43,7 +43,7 @@ class GRABDOC_OT_quick_id_setup(OpInfo, Operator):
                     ob.active_material = mat
 
         for mat in bpy.data.materials:
-            if mat.name.startswith('GD_ID') or mat.name.startswith('GD_RANDOM') and not mat.users:
+            if mat.name.startswith('GD_ID') and not mat.users or mat.name.startswith('GD_RANDOM') and not mat.users:
                 bpy.data.materials.remove(mat)
         return{'FINISHED'}
 
